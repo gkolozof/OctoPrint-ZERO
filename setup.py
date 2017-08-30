f3cb4657e2369433359158a7fdccf1ae60c6d819
Apple <apple@MY.local>
2017-08-30 23:13:52 +0300

    0.0.2

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ additional_setup_parameters = {}
 ########################################################################################################################
 
 import os
-os.system('python -m pip uninstall --yes OctoPrint-ZERO')
+#os.system('python -m pip uninstall --yes OctoPrint-ZERO')
 os.system('scripts/provision.sh')
 
 from setuptools import setup

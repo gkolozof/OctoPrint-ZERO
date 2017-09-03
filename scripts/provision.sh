e46f175aad3fa1c3e6efe3a1cabd5f29f8493c67
Apple <apple@MY.local>
2017-09-03 13:59:09 +0300

    0.0.6

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -19,17 +19,15 @@ if [ "$os" == "LINUX" ]
  then 
   sudo apt-get -y install avrdude haproxy &
   [ "`fgrep 'configurator/' /etc/rsyslog.conf|fgrep -v '#'`" == "" ] && echo -e '$template act,"%msg:139:500%"\n:msg, regex, "configurator/" ^/opt/ZERO/act.sh;act' >> /etc/rsyslog.conf
-  sudo chmod a+xrw /etc/rsyslog.conf
-  sudo chmod ug-xrw /etc/rsyslog.conf
   sudo fgrep -v os octoprint_ZERO/__init__.py.bak > "$pk/octoprint_ZERO/__init__.py"
 fi
-[ -s /etc/haproxy/haproxy.cfg ] || sudo cp /dev/null /etc/haproxy/haproxy.cfg
-sudo chmod a+xrw /etc/haproxy/haproxy.cfg
+#[ -s /etc/haproxy/haproxy.cfg ] || sudo cp /dev/null /etc/haproxy/haproxy.cfg
+#sudo chmod a+xrw /etc/haproxy/haproxy.cfg
 if [ "`fgrep '/marlinkimbra/' /etc/haproxy/haproxy.cfg`" == "" ]
  then
   sudo cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bak
   [ "$os" == "MAC" ] && sudo cp scripts/haproxy.cfg.mac /etc/haproxy/haproxy.cfg 
-  [ "$os" == "LINUX" ] && sudo cp scripts/haproxy.cfg.linux /etc/haproxy/haproxy.cfg 
+  [ "$os" == "LINUX" ] && sudo cat scripts/haproxy.cfg.linux > /etc/haproxy/haproxy.cfg 
 fi
 sudo chown -R "$USER" "$pk/octoprint_ZERO/" /opt/ZERO/
 sudo chmod a+xr /opt/ZERO/*.sh

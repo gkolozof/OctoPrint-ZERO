22d306977b6669ce2e43060284257f9019d14695
Apple <apple@MY.local>
2017-09-03 13:14:47 +0300

    0.0.4

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -31,7 +31,7 @@ if [ "`fgrep '/marlinkimbra/' /etc/haproxy/haproxy.cfg`" == "" ]
   sudo cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bak
   [ "$os" == "MAC" ] && sudo cp scripts/haproxy.cfg.mac /etc/haproxy/haproxy.cfg 
   [ "$os" == "LINUX" ] && sudo cp scripts/haproxy.cfg.linux /etc/haproxy/haproxy.cfg 
-  sudo /etc/init.d/haproxy restart
+  (sleep 60;sudo /etc/init.d/haproxy restart)
 fi
 sudo chown -R "$USER" "$pk/octoprint_ZERO/" /opt/ZERO/
 sudo chmod a+xr /opt/ZERO/*.sh
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# encoding=utf-8
+# coding=utf-8
 
 plugin_version = "0.0.4"
 

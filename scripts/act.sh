6d3e3cc16cf7bc201ad424f2ff66483504f90362
Apple <apple@MY.local>
2017-09-05 20:03:06 +0300

    0.1.2

--- scripts/act.sh
+++ scripts/act.sh
@@ -2,7 +2,7 @@
 [ "`ps ax|fgrep '/bin/bash /opt/ZERO/act.sh'|fgrep -v grep|wc -l`" -gt "2" ] && exit
 echo "<br>" > /opt/ZERO/update
 fw="`echo $*|grep -oP '/configurator\K.*(?= )'`"
-[ "`echo \"$fw\"|fgrep MK4duo`" == "" ] && exit
+[ "`echo \"$fw\"|fgrep MK4duo`" ] || exit
 echo "<pre class='ui-pnotify ui-pnotify-shadow' aria-live='assertive'  style='width:800px;height: 400px;overflow: scroll; background-size: 46%,46%;  background-color: #083142; background-image: url(/plugin/ZERO/static/loading.gif);  color:#ffffcf; background-repeat: no-repeat; background-attachment: fixed;background-position: 52% 29%; ' >" > /opt/ZERO/update
 com="`ls /dev/ttyACM* /dev/ttyUSB*|head -n 1`"
 echo "Disconnecting 3D PRINTER from port $com, Compiling Arduino MEGA2560 & compatible Wait!!!" >> /opt/ZERO/update

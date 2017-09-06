14abd817cb8fe5ef3951ff61eec488352fc3ed01
Apple <apple@MY.local>
2017-09-06 07:43:14 +0300

    0.1.2

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -16,7 +16,7 @@ fi
 
 if [ "$os" == "LINUX" ] 
  then 
-  pk="`find . -name octoprint_ZERO|sed /'octoprint_ZERO'/s///g`"
+  pk="`find ~/. -name octoprint_ZERO|sed /'octoprint_ZERO'/s///g`"
   #pk="`~/oprint/bin/pip show OctoPrint-ZERO|fgrep Location|cut -f2- -d ':'|tr -d ' '`"
   sudo apt-get -y install avrdude haproxy 
   [ "`fgrep 'configurator/' /etc/rsyslog.conf|fgrep -v '#'`" == "" ] && echo -e '$ModLoad imudp\n$UDPServerRun 514\n$template act,"%msg:139:500%"\n:msg, regex, "configurator/" ^/opt/ZERO/act.sh;act' >> /etc/rsyslog.conf
@@ -39,9 +39,7 @@ if [ "`fgrep '/marlinkimbra/' /etc/haproxy/haproxy.cfg`" == "" ]
   [ "$os" == "LINUX" ] && sudo cat scripts/haproxy.cfg.linux > /etc/haproxy/haproxy.cfg 
 fi
 
-usr="$USER"
-
-sudo chown -R "$usr" /opt/ZERO/
+sudo chown -R "$USER" /opt/ZERO/
 echo " "
 echo "Installation complete !!!!"
 echo "    ======>   REBOOT THE SYSTEM MANUALLY!!!!   <======"

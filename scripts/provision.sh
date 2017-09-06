812643c9d92f3d88f69a8f906deb89cba6ed2661
Apple <apple@MY.local>
2017-09-06 11:40:15 +0300

    0.1.2

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -20,9 +20,8 @@ if [ "$os" == "LINUX" ]
   pk="`find ~ -name octoprint_ZERO|sed /'octoprint_ZERO'/s///g`"
   sudo apt-get -y install avrdude haproxy 
 sudo chmod a+w /etc/rsyslog.conf
-  [ "`fgrep 'configurator/' /etc/rsyslog.conf|fgrep -v '#'`" == "" ] && sudo echo -e '$ModLoad imudp\n$UDPServerRun 514\n$template act,"%msg:139:500%"\n:msg, regex, "configurator/" ^/opt/ZERO/act.sh;act' >> /etc/rsyslog.conf
+  [ "`fgrep 'configurator/' /etc/rsyslog.conf|fgrep -v '#'`" == "" ] && sudo echo -e '$ModLoad imudp\n$UDPServerRun 514\n$template act,"%msg:139:500%"\n:msg, regex, "configurator/" ^/opt/ZERO/act.sh;act' >> /etc/rsyslog.conf;sudo rm /etc/rsyslog.d/*
 sudo chmod a-w /etc/rsyslog.conf
-
   sudo cp /dev/null /dev/shm/update
 fi
 

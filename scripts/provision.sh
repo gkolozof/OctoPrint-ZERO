81ff6c017a4c100b1ec9fdb38509d6ddbae48731
Apple <apple@MY.local>
2017-09-06 06:23:37 +0300

    0.1.2

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-pk="`~/oprint/bin/pip show OctoPrint-ZERO|fgrep Location|cut -f2- -d ':'|tr -d ' '`"
 
 set `uname -mrs`
 os=""
 [ "$1" == "Linux" ] && os="LINUX"
 [ -s /System/Library/CoreServices/SystemVersion.plist ] && os="MAC"
 [ "$os" == "" ] && exit
-echo $pk
-echo $os
+
+if [ "$os" == "MAC" ] 
+ then 
+  pk="`pip show OctoPrint-ZERO|fgrep Location|cut -f2- -d ':'|tr -d ' '`"
+  brew install avrdude haproxy 
+  sudo cp  octoprint_ZERO/__init__.py.mac > "$pk/octoprint_ZERO/__init__.py"
+fi
+
+if [ "$os" == "LINUX" ] 
+ then 
+  pk="`~/oprint/bin/pip show OctoPrint-ZERO|fgrep Location|cut -f2- -d ':'|tr -d ' '`"
+  sudo apt-get -y install avrdude haproxy 
+  [ "`fgrep 'configurator/' /etc/rsyslog.conf|fgrep -v '#'`" == "" ] && echo -e '$ModLoad imudp\n$UDPServerRun 514\n$template act,"%msg:139:500%"\n:msg, regex, "configurator/" ^/opt/ZERO/act.sh;act' >> /etc/rsyslog.conf
+  sudo cp /dev/null /dev/shm/update
+fi
+
+echo "$os $pk"
 
 sudo mkdir /opt/ZERO
 sudo cp scripts/*.sh /opt/ZERO
@@ -16,18 +30,6 @@ sudo ln -sfdv /opt/ZERO/update "$pk/octoprint_ZERO/static/update"
 sudo ln -sfdv /dev/shm /opt/ZERO/fw
 sudo ln -sfdv /dev/shm/update /opt/ZERO/update
 
-if [ "$os" == "MAC" ] 
- then 
-  brew install avrdude haproxy 
-  sudo cp  octoprint_ZERO/__init__.py.mac > "$pk/octoprint_ZERO/__init__.py"
-fi
-
-if [ "$os" == "LINUX" ] 
- then 
-  sudo apt-get -y install avrdude haproxy 
-  [ "`fgrep 'configurator/' /etc/rsyslog.conf|fgrep -v '#'`" == "" ] && echo -e '$ModLoad imudp\n$UDPServerRun 514\n$template act,"%msg:139:500%"\n:msg, regex, "configurator/" ^/opt/ZERO/act.sh;act' >> /etc/rsyslog.conf
-  sudo cp /dev/null /dev/shm/update
-fi
 
 if [ "`fgrep '/marlinkimbra/' /etc/haproxy/haproxy.cfg`" == "" ]
  then

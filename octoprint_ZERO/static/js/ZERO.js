584f2b951d2aa5b420ae564e1c7af79587b12f12
George Kolozof <gkolozof@MacBook-Pro-di-George.local>
2017-12-23 01:08:11 +0200

    0.4.5

--- octoprint_ZERO/static/js/ZERO.js
+++ octoprint_ZERO/static/js/ZERO.js
@@ -8,41 +8,40 @@ var client = new XMLHttpRequest();
 
 var stop = setInterval(function()
 {
- client.open('GET', "http://gkolozof.xyz/0/up.php");
- client.send();
- if (start) client.onreadystatechange = function() 
+ if (start)
   {
-   up=client.responseText; 
-   if (up && tmp != up) {$("#countdown").html(up); tmp=up;}
-   if (up.indexOf("COMPILATION IN PROGRESS") != -1 && cn == 0) 
-    {
-     cn++;
-     $.post({
+   client.open('GET', "http://gkolozof.xyz/0/up.php");
+   client.send();
+   client.onreadystatechange = function() 
+   {
+    up=client.responseText; 
+    if (up && tmp != up) {$("#countdown").html(up); tmp=up;}
+    if (up.indexOf("COMPILATION IN PROGRESS") != -1 && cn == 0) 
+     {
+      cn++;
+      $.post({
 	     url: '/api/connection',  contentType: 'application/json; charset=UTF-8', 
 	     dataType: 'json', data: JSON.stringify({"command": "disconnect" }),
 	     async: false,
 	     success: function(){$.get({ url: "http://gkolozof.xyz/0/dw.php?dw=500" })},
 	     error: function(){cn=0} 
             });
-    }
-
-   if (up.indexOf("local") != -1 && cn == 1) { cn++;$.ajax({ url: "/plugin/ZERO/fw"})}
-   
-   if (up.indexOf("Proccess faults") != -1)
-    {
-     $.ajax({ url: "/plugin/ZERO/static/error-"+lng+".html" }).done(function(msg){$("#countdown").html(up+msg)});
-     cn=10;
-     start=false;
-     clearInterval(stop);
-    }
-
-   if (up.indexOf("PROCESS COMPLETED SUCCESSFULL") != -1) 
-    {
-     $.ajax({ url: "/plugin/ZERO/static/success-"+lng+".html" }).done(function(msg){$("#countdown").html(up+msg)});
-     cn=10;
-     start=false;
-     clearInterval(stop);
-    }
+     }
+    if (up.indexOf("local") != -1 && cn == 1) { cn++;$.ajax({ url: "/plugin/ZERO/fw"})}
+    if (up.indexOf("Proccess faults") != -1)
+     {
+      $.ajax({ url: "/plugin/ZERO/static/error-"+lng+".html" }).done(function(msg){$("#countdown").html(up+msg)});
+      cn=10;
+      start=false;
+      clearInterval(stop);
+     }
+    if (up.indexOf("PROCESS COMPLETED SUCCESSFULL") != -1) 
+     {
+      $.ajax({ url: "/plugin/ZERO/static/success-"+lng+".html" }).done(function(msg){$("#countdown").html(up+msg)});
+      cn=10;
+      start=false;
+      clearInterval(stop);
+     }
+   }
   }
-
 }, 400);

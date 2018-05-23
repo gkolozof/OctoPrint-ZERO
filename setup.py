ce0b4aa1973ec3d6b76ace96673b959559c95a28
George Kolozof <gkolozof@gmail.com>
2018-05-23 03:17:13 +0300

    0.5.14

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-plugin_version = "0.5.13"
+plugin_version = "0.5.14"
 
 ########################################################################################################################
 ### Do not forget to adjust the following variables to your own plugin.
